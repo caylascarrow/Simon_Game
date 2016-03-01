7f0582cd104122c88e6b6b9f87de39b5ac749301
Cayla Scarrow <yaas.cs21@hotmail.com>
2016-03-01 10:01:30 -0500

    Started programming button colour changes in computer's turn

--- SimonGame/GameScreen.cs
+++ SimonGame/GameScreen.cs
@@ -5,17 +5,15 @@ using System.Drawing;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SimonGame
 {
     public partial class GameScreen : UserControl
     {
-
+        //make variables local to this screen
         int colourVal;
-
-        string colourName;
 
         Random randNum = new Random();
 
@@ -23,12 +21,46 @@ namespace SimonGame
         {
             InitializeComponent();
 
+            //clear list
+            Form1.pattern.Clear();
+
+            //go to computer's turn method
             ComputerTurn(randNum, colourVal);
         }
         public void ComputerTurn(Random randNum, int colourVal)
         {
+            //remove "this" control and make "f" control
+            Form f = this.FindForm();
+            f.Controls.Remove(this);
+
+            //make colourVal equal to a random number
             colourVal = randNum.Next(1, 5);
 
+            //add random number into list
+            Form1.pattern.Add(colourVal); 
+
+            //make buttons light up as the colours
+            for(int i = -1; i< Form1.pattern.Count; i++)
+            {
+                if(Form1.pattern[i] == 1)
+                {
+                    buttonYellow.ForeColor = Color.Yellow;
+
+                    f.Refresh();
+                    Thread.Sleep(500);
+
+                    buttonYellow.ForeColor = Color.SlateBlue;
+                }
+                if(Form1.pattern[i] == 2)
+                {
+                    buttonBlue.ForeColor = Color.Blue;
+
+                    f.Refresh();
+                    Thread.Sleep(500);
+
+                    buttonBlue.ForeColor = Color.SlateBlue;
+                }
+            }
         }
     }
 }
